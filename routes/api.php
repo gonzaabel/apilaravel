d7b577a4b977ba3133ef5f51f43e1966268e3e89
gonzaabel <gonzaloabel@hotmail.com>
2020-11-07 17:38:05 -0300

    moving routes of the API to the correct file to be accessed through AJAX

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Resources\User as UserResource;
+use App\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,44 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:api')->get('/users', function () {
+    return new UserResource(User::all());
+});
+
+Route::middleware('auth:api')->post('/users/create', function(Request $request){
+
+	$rta = new UserResource(User::create($request->all()));
+
+	if(!empty($rta->resource)) {
+		return "{msg: 'User succesfully created'}";
+	}
+
+	return "{msg: 'Error: User could not be created.'}";
+
+});
+
+Route::middleware('auth:api')->post('/users/update', function(Request $request){
+
+	$rta = new UserResource(User::save($request->all()));
+
+	if(!empty($rta->resource)) {
+		return "{msg: 'User succesfully updated'}";
+	}
+
+	return "{msg: 'Error: User could not be updated.'}";
+
+});
+
+Route::middleware('auth:api')->get('/users/delete/{id}', function($id){
+
+	$rta = new UserResource(User::destroy($id));
+
+	if(!empty($rta->resource)) {
+		return "{msg: 'User succesfully deleted'}";
+	}
+
+	return "{msg: 'Error: User could not be deleted.'}";
+
 });
